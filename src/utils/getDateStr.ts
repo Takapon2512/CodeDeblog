fc9df27ec9ef6d7dc2c399b7d996b83f7507b614
たかぽん <takapontype0924@gmail.com>
2023-06-02 14:46:58 +0900

    ver 1.17

--- src/utils/getDateStr.ts
+++ src/utils/getDateStr.ts
@@ -1,3 +1,5 @@
-// export const getDateStr = (date: string) => {
-//     return new Date(date).toLocaleDateString()
-// }
+export const getDateStr = (date: string) => {
+    return new Date(date).toLocaleDateString('ja-JP', {
+        timeZone: 'Asia/Tokyo'
+    })
+}
